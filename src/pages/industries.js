69405e8e953e8e1e824b46095576d8eb02af4dcb
DelrosarioRovic <rovicdelrosario01@gmail.com>
2025-10-31 13:34:47 +0800

    fix

--- src/pages/industries.js
+++ src/pages/industries.js
@@ -1,12 +1,21 @@
 import Introduction from "@/component/industries/Introduction";
 import ListIndustries from "@/component/industries/ListIndustries";
+import SEO from "@/utility/useSeo";
 
 export default function Industries() {
   return (
-    <div className="flex flex-col 
-      lg:gap-20 md:gap-10 max-md:gap-10 max-sm:gap-5">
+    <>
+      <SEO
+        title="Lead Sourze | Industries We Serve – Telesales & Outbound Calling for NZ & AU Businesses"
+        description="See how Lead Sourze supports a wide range of industries—from home services and real estate to healthcare and solar—with tailored outbound calling, lead generation and appointment‑setting solutions for New Zealand and Australia."
+        url={`${process.env.NEXT_PUBLIC_BASE_URL}`}
+       />
+      <div className="flex flex-col 
+        lg:gap-20 md:gap-10 max-md:gap-10 max-sm:gap-5">
         <Introduction />
         <ListIndustries />
-    </div>
+      </div>
+    </>
+    
   );
 }
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,16 +1,25 @@
 import Introduction from "@/component/services/Introduction";
 import ITServices from "@/component/services/ITservices";
 import ServicesList from "@/component/services/ServicesList";
+import SEO from "@/utility/useSeo";
 
 export default function Services() {
   return (
-    <div className="flex flex-col 
-      lg:gap-20 md:gap-10 max-md:gap-10 max-sm:gap-5">
+    <>
+      <SEO
+        title="Lead Sourze | Our Services – Outbound Calling, Appointment Setting & Lead Generation NZ & AU"
+        description="Explore Lead Sourze’s full suite of services: outbound calling, qualified lead generation, appointment setting, database cleansing and more for businesses across New Zealand and Australia."
+        url={`${process.env.NEXT_PUBLIC_BASE_URL}`}
+       />
+      <div className="flex flex-col 
+        lg:gap-20 md:gap-10 max-md:gap-10 max-sm:gap-5">
         <div>
-            <Introduction />
-            <ServicesList />
-            <ITServices />
+          <Introduction />
+          <ServicesList />
+          <ITServices />
         </div>
-    </div>
+      </div>
+    </>
+    
   );
 }
